9bd1036a1e864110a50de23c9ae223bce8a50073
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-11-07 10:01:37 +0800

    update handler.js

--- handler.js
+++ handler.js
@@ -399,7 +399,7 @@ module.exports = {
                 .setGuildIcon(ppgc)
                 .setMemberCount(groupMetadata.participants.length)
                 .setAvatar(pp)
-                .setBackground("https://telegra.ph/file/8ad987e2a120714cbae21.jpg")
+                .setBackground("https://telegra.ph/file/562d34d6617952eb3566e.jpg")
                 .toAttachment()
 
               let lea = await new knights.Goodbye()
@@ -408,7 +408,7 @@ module.exports = {
                 .setGuildIcon(ppgc)
                 .setMemberCount(groupMetadata.participants.length)
                 .setAvatar(pp)
-                .setBackground("https://telegra.ph/file/7788576b8e14ad52634b3.jpg")
+                .setBackground("https://telegra.ph/file/562d34d6617952eb3566e.jpg")
                 .toAttachment()
 
               this.sendButtonLoc(jid, action === 'add' ? wel.toBuffer() : lea.toBuffer(), text, action === 'add' ? '●○●○●○●○' : '●○●○●○●○', action === 'add' ? 'Selamat Bergabung' : 'Sayonara',action === 'add' ? 'Selamat Bergabung' : 'Sampai Jumpa Kembali', null, {
